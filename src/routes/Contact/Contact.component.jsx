c85d176913379e4d12c4f1b20e0a19d7212f8b5f
Gustavo Canello <gustavobrandicanello@gmail.com>
2023-03-15 13:54:44 -0300

    Adjusting pop up paddings.

--- src/routes/Contact/Contact.component.jsx
+++ src/routes/Contact/Contact.component.jsx
@@ -6,10 +6,9 @@ import { CONTACT } from "../../data/contact.data";
 export const Contact = () => {
     return (
         <div className="Contact">
-            <span className="text-info">
-                gustavobrandicanello@gmail.com&nbsp;&nbsp;|&nbsp;&nbsp;+55 11
-                96049 4096
-            </span>
+            <span className="text-info">+55 11 96049 4096</span>
+            <Spacer y="4px" />
+            <span className="text-info">gustavobrandicanello@gmail.com</span>
             <Spacer y="48px" />
             <div className="info-cards-container">
                 <ContactCard media={CONTACT.linkedin} />
