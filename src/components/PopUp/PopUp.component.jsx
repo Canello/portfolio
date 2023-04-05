f2186451d4eb7e02de56e3768c50d72916104ece
Gustavo Canello <gustavobrandicanello@gmail.com>
2023-04-05 13:42:14 -0300

    First try adding animation to pop up opening/closing.

--- src/components/PopUp/PopUp.component.jsx
+++ src/components/PopUp/PopUp.component.jsx
@@ -1,19 +1,28 @@
 import "./PopUp.styles.scss";
 import { Spacer } from "../Spacer/Spacer.component";
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { PopUpContext } from "../../contexts/PopUpContext.context";
 import { CloseIcon } from "./subcomponents/CloseIcon.component";
 
 export const PopUp = ({ popUp }) => {
     const { id, z, title, content } = popUp;
     const { closePopUp } = useContext(PopUpContext);
+    const ref = useRef();
+
+    const animateThenClose = (id) => {
+        ref.current.classList.add("closing");
+        setTimeout(() => closePopUp(id), 150);
+    };
 
     return (
         <div className="PopUp" style={{ zIndex: z }}>
-            <div className="pop-up-background" onClick={() => closePopUp(id)} />
-            <div className="pop-up-wrapper">
+            <div
+                className="pop-up-background"
+                onClick={() => animateThenClose(id)}
+            />
+            <div className="pop-up-wrapper" ref={ref}>
                 <h2 className="pop-up-title">{title}</h2>
-                <CloseIcon onClick={() => closePopUp(id)} />
+                <CloseIcon onClick={() => animateThenClose(id)} />
                 <Spacer y="40px" />
                 {content}
             </div>
